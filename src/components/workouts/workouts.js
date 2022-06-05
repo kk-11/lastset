205f7e08848622156151d1fc79ae0f31564fee18
killian <killian.debuitleir@gmail.com>
2022-06-05 15:44:13 +0200

    more checks

--- src/components/workouts/workouts.js
+++ src/components/workouts/workouts.js
@@ -29,7 +29,8 @@ export default function Workouts() {
 
 	return (
 		<div className={s.wrapper}>
-			{(workouts && !workouts[activeWorkoutIdx]) ||
+			{workouts.length === 0 ||
+			!workouts[activeWorkoutIdx] ||
 			activeWorkoutIdx === null ? (
 				<>
 					{workouts.map(({ name }, idx) => (
